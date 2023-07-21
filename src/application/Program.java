a0d8b5ca18fad29b85f38faca918728c15a4d0c0
Jarbas Santos <jarbas3@outlook.com>
2023-07-21 09:09:07 -0300

    Interface Function com expressão lambda declarada

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,10 +4,10 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
-import util.UpperCaseName;
 
 public class Program {
 	
@@ -21,8 +21,10 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
+		Function<Product, String> func = p -> p.getName().toUpperCase();
+		
 		//List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
-		List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+		List<String> names = list.stream().map(func).collect(Collectors.toList());
 		names.forEach(System.out::println);
 	}
 }
